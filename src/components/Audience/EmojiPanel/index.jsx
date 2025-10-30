7dfaa8e2afe52594fd67de02e39e79c9d7696b72
wonpark1 <2023111927@dgu.ac.kr>
2025-10-30 20:14:34 +0900

    feat: emojipanel 기본 아이콘 넣기

--- src/components/Audience/EmojiPanel/index.jsx
+++ src/components/Audience/EmojiPanel/index.jsx
@@ -6,18 +6,25 @@ import {
   EmojiIcon,
   SpacingBox,
 } from "./EmojiPanel.styles";
-import goodSVG from "../../../assets/images/good.svg";
+import interestSVG from "../../../assets/icons/Emoji/Interesting.png";
+import surpriseSVG from "../../../assets/icons/Emoji/Surprising.png";
+import curiousSVG from "../../../assets/icons/Emoji/Curious.png";
+import excitingSVG from "../../../assets/icons/Emoji/Exciting.png";
+import angrySVG from "../../../assets/icons/Emoji/Angry.png";
+import sadSVG from "../../../assets/icons/Emoji/Sad.png";
+import okSVG from "../../../assets/icons/Emoji/O.png";
+import xSVG from "../../../assets/icons/Emoji/X.png";
 
 const EmojiPanel = () => {
   const emojis = [
-    { id: 1, icon: goodSVG },
-    { id: 2, icon: goodSVG },
-    { id: 3, icon: goodSVG },
-    { id: 4, icon: goodSVG },
-    { id: 5, icon: goodSVG },
-    { id: 6, icon: goodSVG },
-    { id: 7, icon: goodSVG },
-    { id: 8, icon: goodSVG },
+    { id: 1, icon: interestSVG },
+    { id: 2, icon: surpriseSVG },
+    { id: 3, icon: curiousSVG },
+    { id: 4, icon: excitingSVG },
+    { id: 5, icon: angrySVG },
+    { id: 6, icon: sadSVG },
+    { id: 7, icon: okSVG },
+    { id: 8, icon: xSVG },
   ];
 
   return (
